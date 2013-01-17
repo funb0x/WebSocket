68169dfab687d89604c1de7733c0332a8a2ce24b
funb0x <oleg.tymchak@gmail.com>
2013-01-17 15:16:53 +0200

    working chat

--- src/main/java/com/tymchak/SSEAtmosphereHandler.java
+++ src/main/java/com/tymchak/SSEAtmosphereHandler.java
@@ -11,63 +11,54 @@ import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor;
 import java.io.IOException;
 import java.util.Date;
 
-/**
- * Simple AtmosphereHandler that implement the logic to build a Server Side
- * Events Chat application.
- * 
- * @author Jeanfrancois Arcand
- */
-@AtmosphereHandlerService(path = "/chat", interceptors = { AtmosphereResourceLifecycleInterceptor.class })
+@AtmosphereHandlerService(path = "/chat", interceptors= {AtmosphereResourceLifecycleInterceptor.class})
 public class SSEAtmosphereHandler implements AtmosphereHandler {
 
-	@Override
-	public void onRequest(AtmosphereResource r) throws IOException {
+	 @Override
+	    public void onRequest(AtmosphereResource r) throws IOException {
 
-		AtmosphereRequest req = r.getRequest();
-		if (req.getMethod().equalsIgnoreCase("POST")) {
-			r.getBroadcaster().broadcast(req.getReader().readLine().trim());
-		}
-	}
+	        AtmosphereRequest req = r.getRequest();
+	        if (req.getMethod().equalsIgnoreCase("POST")) {
+	            r.getBroadcaster().broadcast(req.getReader().readLine().trim());
+	        }
+	    }
 
-	@Override
-	public void onStateChange(AtmosphereResourceEvent event) throws IOException {
-		AtmosphereResource r = event.getResource();
-		AtmosphereResponse res = r.getResponse();
+	    @Override
+	    public void onStateChange(AtmosphereResourceEvent event) throws IOException {
+	        AtmosphereResource r = event.getResource();
+	        AtmosphereResponse res = r.getResponse();
 
-		if (event.isSuspended()) {
-			String body = event.getMessage().toString();
+	        if (event.isSuspended()) {
+	            String body = event.getMessage().toString();
 
-			// Simple JSON -- Use Jackson for more complex structure
-			// Message looks like { "author" : "foo", "message" : "bar" }
-			String author = body.substring(body.indexOf(":") + 2,
-					body.indexOf(",") - 1);
-			String message = body.substring(body.lastIndexOf(":") + 2,
-					body.length() - 2);
+	            // Simple JSON -- Use Jackson for more complex structure
+	            // Message looks like { "author" : "foo", "message" : "bar" }
+	            String author = body.substring(body.indexOf(":") + 2, body.indexOf(",") - 1);
+	            String message = body.substring(body.lastIndexOf(":") + 2, body.length() - 2);
 
-			res.getWriter().write(new Data(author, message).toString());
-		} else if (!event.isResuming()) {
-			event.broadcaster().broadcast(
-					new Data("Someone", "say bye bye!").toString());
-		}
-	}
+	            res.getWriter().write(new Data(author, message).toString());
+	        } else if (!event.isResuming()){
+	            event.broadcaster().broadcast(new Data("Someone", "say bye bye!").toString());
+	        }
+	    }
 
-	@Override
-	public void destroy() {
-	}
+	    @Override
+	    public void destroy() {
+	    }
 
-	private final static class Data {
+	    private final static class Data {
 
-		private final String text;
-		private final String author;
+	        private final String text;
+	        private final String author;
 
-		public Data(String author, String text) {
-			this.author = author;
-			this.text = text;
-		}
+	        public Data(String author, String text) {
+	            this.author = author;
+	            this.text = text;
+	        }
 
-		public String toString() {
-			return "{ \"text\" : \"" + text + "\", \"author\" : \"" + author
-					+ "\" , \"time\" : " + new Date().getTime() + "}";
-		}
-	}
+	        public String toString() {
+	            return "{ \"text\" : \"" + text + "\", \"author\" : \"" + author + "\" , \"time\" : " + new Date().getTime() + "}";
+	        }
+	    }
+
 }
